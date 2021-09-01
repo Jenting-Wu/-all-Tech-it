22af49b371094e9e7025373b15962cd7e90d82d5
lecy1219@gmail.com <cy820212@gmail.com>
2021-09-02 00:03:41 +0800

    1203 contant +foot.php

---
+++ contant.php
@@ -0,0 +1,44 @@
+<?php require_once 'head.php' ?>
+<title>客服咨詢</title>
+<link rel="stylesheet" href="CSS/contant.css">
+
+<div class="wrap">
+    <div class="container_self">
+        <h4>與TECHIT聯絡</h4>
+        <h5>銷售與產品諮詢</h5>
+        <div class="main">
+            <div class="l_part">
+                <h6>TECHIT Online Store</h6>
+                <p>你可前往 TECHIT Online Store 購買 TECHIT 硬體、軟體與第三方配件產品。若要透過電話購買，請致電 TECHIT 客戶服務中心 0800-120-021。</p>
+                <p>
+                    若要查看訂單最新狀態及變更你的 TECHIT Online Store 訂單，請瀏覽 <a href="member_info3.php"></a>
+                    訂單狀態網頁。你也可致電 TECHIT Online Store 客戶服務中心 0800-120-021，或瀏覽線上輔助說明，以取得進一步資訊。</p>
+            </div>
+            <div class="r_part">
+                <h6>瀏覽線上支援</h6>
+                <p>需要服務或支援？立即在線上提出請求，我們將為你尋找解決方案。您可使用<a href="mailto:iamsophia0903@gmail.com">聯絡我們</a>將有專人爲您服務
+
+                </p>
+
+            </div>
+        </div>
+
+
+        <h5>產品與技術支援</h5>
+        <!-- <div class="main">
+            <div class="l_part"> -->
+        <h6>聯絡 Apple 支援</h6>
+        <p>需要服務或支援？立即在線上提出請求，我們將為你尋找解決方案。</p>
+        <p>更多取得協助的方式：</p>
+
+        <ul>
+            <li>技術支援：0812 015 923</li>
+            <li>服務時間：週一至週五 09:00 - 18:00 </li>
+            <li>服務時間：24小時全年無休</li>
+            <li>聯絡方式：<a href="mailto:iamsophia0903@gmail.com">iamsophia0903</a></li>
+            <li>公司負責人：Sophia Lin</li>
+        </ul>
+    </div>
+</div>
+<!-- php link -------------------------------------------->
+<?php require_once 'foot.php' ?>
